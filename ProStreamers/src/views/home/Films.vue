b7443f8c5636cd8c7a87dbd841aade8a5e26e2cb
chaeriz <chrisobt@outlook.com>
2024-06-14 13:12:01 +0200

    Add api fetch in Films page

--- ProStreamers/src/views/home/Films.vue
+++ ProStreamers/src/views/home/Films.vue
@@ -1,8 +1,42 @@
 <template>
     <div class="Films">
-        <RouterLink style="float:right" to="/"><i class="fa-solid fa-circle-xmark"></i></RouterLink>
+        <router-link style="float:right" to="/"><i class="fa-solid fa-circle-xmark"></i></router-link>
         <h1>Films</h1>
+
+        <ul>
+            <li v-for="film in films" :key="film.id">
+                {{ film.title }}
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            films: []
+        };
+    },
+    mounted() {
+        this.fetchFilms();
+    },
+    methods: {
+        fetchFilms() {
+            const url = "http://chrisouboter.com/api/movies/all";
+
+            axios.get(url)
+                .then(response => {
+                    this.films = response.data;
+                })
+                .catch(error => {
+                    console.error('Error fetching films', error);
+                });
+        }
+    }
+};
+</script>
 
 <style></style>
