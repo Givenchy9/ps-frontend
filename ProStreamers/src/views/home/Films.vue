e30c6b1272ad4bd90947327ccb0ce4e2c24c3dc7
chaeriz <chrisobt@outlook.com>
2024-06-14 13:47:01 +0200

    Fetch API

--- ProStreamers/src/views/home/Films.vue
+++ ProStreamers/src/views/home/Films.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="Films">
-        <router-link style="float:right" to="/"><i class="fa-solid fa-circle-xmark"></i></router-link>
+        <router-link style="float: right;" to="/"><i class="fa-solid fa-circle-xmark"></i></router-link>
         <h1>Films</h1>
 
-        <ul>
-            <li v-for="film in films" :key="film.id">
-                {{ film.title }}
-            </li>
-        </ul>
+        <div class="film-cards">
+            <div v-for="film in films" :key="film.id" class="film-card">
+                <h2>{{ film.title }}</h2>
+                <p>{{ film.movie_type }}</p>
+                <p>{{ film.year }}</p>
+                Minutes: <p>{{ film.minutes }}</p>
+
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -25,7 +30,7 @@ export default {
     },
     methods: {
         fetchFilms() {
-            const url = "http://chrisouboter.com/api/movies/all";
+            const url = "http://127.0.0.1:8000/api/movies/all";
 
             axios.get(url)
                 .then(response => {
@@ -39,4 +44,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.film-cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 20px;
+}
+
+.film-card {
+    background-color: #f0f0f0;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
