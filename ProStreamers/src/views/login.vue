591f47068dbf315ac67cfc22f174dc66411d5221
chaeriz <chrisobt@outlook.com>
2024-06-21 16:54:24 +0200

    CSS update

--- ProStreamers/src/views/login.vue
+++ ProStreamers/src/views/login.vue
@@ -3,7 +3,7 @@
     <div class=" flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         
 
-        <div class="bg-white bg-transparent-10 p-4 rounded-xl mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="bg-white shadow-xl bg-transparent-10 p-4 rounded-xl mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in</h2>
         </div>
@@ -24,13 +24,13 @@
                     </div>
                     <div class="mt-2">
                         <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required=""
-                            class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            class="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-400 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
                 <div>
                     <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        class="flex shadow-xl w-full justify-center rounded-md bg-violet-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-xl hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         <div v-if="!loading">Login</div>
                         <div v-if="loading" class="loading-screen">
         <i class="fa-solid fa-spinner fa-spin-pulse fa-xl"></i>
