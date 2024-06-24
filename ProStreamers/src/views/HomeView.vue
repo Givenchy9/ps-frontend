93733a1e88d4226a95de085666f97c0ceb8b4903
chaeriz <chrisobt@outlook.com>
2024-06-24 19:32:15 +0200

    Header update

--- ProStreamers/src/views/HomeView.vue
+++ ProStreamers/src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
   <header
-    class="flex p-8 w-1/2 m-auto gap-4 text-center justify-center background-black backdrop-blur-sm rounded-sm fixed top-0 left-1/2 transform -translate-x-1/2">
-    <RouterLink
-      class="isolate w-1/3 py-2 px-4 rounded-full bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
+    class="flex p-8 w-full m-auto justify-between gap-4 text-center justify-center background-black backdrop-blur-sm rounded-sm fixed top-0 left-1/2 transform -translate-x-1/2">
+    <div class="font-bold text-2xl">ProStreamers</div>
+    
+    <div><RouterLink
+      class="isolate w-1/3 py-2 px-4 gap-2 rounded-md mx-2 bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
       to="/films" active-class="bg-black/50 underline">Films</RouterLink>
     <RouterLink
-      class="isolate w-1/3 py-2 px-4 rounded-full bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
+      class="isolate w-1/3  py-2 px-4  gap-2 rounded-md mx-2 bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
       to="/series" active-class="bg-black/50 underline">Series</RouterLink>
     <RouterLink
-      class="isolate w-1/3 py-2 px-4 rounded-full bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
-      to="/latest" active-class="bg-black/50 underline">Latest</RouterLink>
+      class="isolate w-1/3  py-2 px-4  gap-2 rounded-md mx-2 bg-white/10 shadow-xl font-bold ring-2 ring-gray-200 hover:bg-black/20 duration-700"
+      to="/latest" active-class="bg-black/50 underline">Latest</RouterLink></div>
+    <div class="flex flex-row gap-2 rounded-md ring-gray-200 hover:bg-black/20 duration-700"><button class="bg-white/10 rounded-md px-4 py-2 flex">   <i class="fa-solid fa-user px-2 fa-md"></i><p class="mx-2 font-bold">Log out</p> <p>{{ username  }}</p></button></div>
+ 
+      
   </header>
 
   <main class="px-4 mt-32 mb-20">
@@ -33,7 +38,7 @@ export default defineComponent({
 
     if (user) {
       user = JSON.parse(user) // Parse the stored JSON string into an object
-      this.username = user.name // Set the username from the parsed object
+      this.username = user?.name // Set the username from the parsed object
     }
   }
 })
