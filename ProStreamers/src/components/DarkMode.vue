c047b3790543baf5dabe68cc7525256e1ccaeb19
Chris Ouboter <129173682+chaeriz19@users.noreply.github.com>
2024-06-28 15:51:17 +0200

    Center dark mode button

--- ProStreamers/src/components/DarkMode.vue
+++ ProStreamers/src/components/DarkMode.vue
@@ -1,33 +1,38 @@
 <template>
-    <div @click="toggleDark()" class="toggle-switch-container flex items-center">
-        <input type="checkbox" id="toggle-switch" v-model="isDark"
-            class="toggle-input appearance-none w-10 h-5 bg-gray-400 rounded-3xl relative cursor-pointer outline-none checked:bg-blue-500">
-    </div>
+  <div @click="toggleDark()" class="toggle-switch-container flex items-center align-middle">
+    <input
+      type="checkbox"
+      id="toggle-switch"
+      v-model="isDark"
+      class="toggle-input appearance-none w-10 h-5 bg-gray-400 rounded-3xl relative cursor-pointer outline-none checked:bg-blue-500"
+    />
+  </div>
 </template>
 
 <script setup>
-import { useDark, useToggle } from "@vueuse/core";
+import { useDark, useToggle } from '@vueuse/core'
 
-const isDark = useDark();
-console.log(isDark.value);
-const toggleDark = useToggle(isDark); 
+const isDark = useDark()
+console.log(isDark.value)
+const toggleDark = useToggle(isDark)
 </script>
 
 <style>
 .toggle-input::before {
-    content: "";
-    position: absolute;
-    width: 18px;
-    height: 18px;
-    border-radius: 50%;
-    background-color: white;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 2px;
-    transition: 0.2s;
+  content: '';
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  background-color: white;
+  top: 50%;
+  transform: translateY(-50%);
+
+  left: 2px;
+  transition: 0.2s;
 }
 
 .toggle-input:checked::before {
-    left: 20px;
+  left: 20px;
 }
 </style>
--- ProStreamers/src/views/HomeView.vue
+++ ProStreamers/src/views/HomeView.vue
@@ -1,24 +1,38 @@
 <template>
   <header
-    class="flex p-8 w-full m-auto justify-between gap-4 text-center justify-center background-black backdrop-blur-sm rounded-sm fixed top-0 left-1/2 transform -translate-x-1/2">
+    class="flex p-8 w-full m-auto justify-between gap-4 text-center justify-center background-black backdrop-blur-sm rounded-sm fixed top-0 left-1/2 transform -translate-x-1/2"
+  >
     <div class="font-bold text-2xl text-black dark:text-white">ProStreamers</div>
 
     <div class="align-middle flex-row flex">
       <RouterLink
         class="isolate text-gray-600 dark:text-white w-1/3 py-2 px-4 gap-2 rounded-md mx-2 bg-white/10 shadow-xl ring-2 ring-gray-200 hover:bg-gray-200 hover:text-gray-600 duration-200"
-        to="/films" active-class="underline font-bold ring-gray-400">Films</RouterLink>
+        to="/films"
+        active-class="underline font-bold ring-gray-400"
+        >Films</RouterLink
+      >
       <RouterLink
         class="isolate text-gray-600 dark:text-white w-1/3 py-2 px-4 gap-2 rounded-md mx-2 bg-white/10 shadow-xl ring-2 ring-gray-200 hover:bg-gray-200 hover:text-gray-600 duration-200"
-        to="/series" active-class="underline font-bold ring-gray-400">Series</RouterLink>
+        to="/series"
+        active-class="underline font-bold ring-gray-400"
+        >Series</RouterLink
+      >
       <RouterLink
-        class="isolate text-gray-600 dark:text-white w-1/3 py-2 px-4 gap-2 rounded-md mx-2 bg-white/10 shadow-xl ring-2 ring-gray-200 hover:bg-gray-200 hover:text-gray-600 duration-200"
-        to="/latest" active-class="underline font-bold ring-gray-400">Latest</RouterLink>
+        class="isolate text-gray-600 dark:text-white align-middle w-1/3 py-2 px-4 gap-2 rounded-md mx-2 bg-white/10 shadow-xl ring-2 ring-gray-200 hover:bg-gray-200 hover:text-gray-600 duration-200"
+        to="/latest"
+        active-class="underline font-bold ring-gray-400 align-middle"
+        >Latest</RouterLink
+      >
     </div>
-    <DarkMode class="absolute right-48" />
-    <div class="flex text-white flex-row gap-2 rounded-md ring-gray-200 hover:bg-black/20 duration-700">
-      <button @click="logOut" class="bg-white/5 rounded-md px-4 py-2 flex justify-center items-center">
-        <i class="fa-solid fa-user px-2 fa-md text-gray-600 dark:text-white align-middle"></i>
-        <p class="mx-2 font-bold align-middle text-gray-600 dark:text-white">Log out</p>
+
+    <div class="flex items-center gap-2 text-white">
+      <DarkMode class="absolute right-48" />
+      <button
+        @click="logOut"
+        class="bg-white/5 rounded-md px-4 py-2 flex justify-center items-center"
+      >
+        <i class="fa-solid fa-user px-2 fa-md text-gray-600 dark:text-white"></i>
+        <p class="mx-2 font-bold text-gray-600 dark:text-white">Log out</p>
       </button>
     </div>
   </header>
