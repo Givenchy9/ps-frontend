75380368d8107600f66b2f992cf62a415e122bdc
chaeriz <chrisobt@outlook.com>
2024-07-02 12:27:11 +0200

    Revert "Call CARD type"
    This reverts commit 66adaaf1805a68c171327e951e3958ab6053b5c0.

--- ProStreamers/src/views/home/Films.vue
+++ ProStreamers/src/views/home/Films.vue
@@ -41,10 +41,9 @@
 </template>
 
 <script lang="ts">
-import FilmCard from '@/components/Card.vue'
 import { ref, onMounted, watch, defineComponent } from 'vue'
 import axios from 'axios'
-
+import FilmCard from '@/components/Card.vue'
 import Footer from '@/components/Footer.vue'
 
 interface Film {
